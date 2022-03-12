4c8b3817c08757341f5ddbb63ae449ffb5093a0d
Ana Vargazon <ana.vargazon@gmail.com>
2022-03-12 12:39:37 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/cart")
+def portfolio():
+    return render_template("cart.html")
+
+
+if __name__ == '__main__':
+    app.run(use_reloader=True, debug=True)
